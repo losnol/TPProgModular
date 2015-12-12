741e981f88c892a8852e0a687379900ada73250f
losnol <brenonardo@hotmail.com>
2015-12-12 18:08:41 -0200

    Novo diagrama e esboço da classe solicitação.

--- src/Solicitacao.java
+++ src/Solicitacao.java
@@ -1,3 +1,17 @@
-public class Solicitacao {
+public class Solicitacao { //TODO
+	private String tipoProfissional;
+	private String descricao;
+	private Cliente cliente;
+	public Solicitacao(){
+		tipoProfissional = "NULL";
+		descricao = "NULL";
+		cliente = new Cliente();
+	}
+	public Solicitacao(String tipoProfissional, String descricao, Cliente cliente){
+		this.tipoProfissional = tipoProfissional;
+		this.descricao = descricao;
+		this.cliente = cliente;
+	
+	}
 
 }
