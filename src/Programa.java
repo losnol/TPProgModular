1607281a119b35e15db2f832e629d963164f6ba7
vinisi <vini22004@gmail.com>
2015-12-14 20:27:32 -0200

    Inserindo no BD

--- src/Programa.java
+++ src/Programa.java
@@ -1,4 +1,7 @@
 //Main:
+
+import java.sql.*;
+
 public class Programa {
 	
 	public static void main(String[] args) {
@@ -11,6 +14,18 @@ public class Programa {
 		A.consultarClientes();
 		//JanelaAdicaoCliente D =  new JanelaAdicaoCliente();
 		//D.setVisible(true);
+		
+        ConnectionFactory.getConnection();
+        System.out.println(ConnectionFactory.status); //imprime o valor de status
+
+        Cliente cliente = new Cliente("nome teste", "086.669.686-56", "3476-3915", "mg-14.853.816", "TESTE", "vini22004@gmail.com");
+        
+        System.out.println(cliente.getNome());
+        
+        ClienteDao clienteDao = new ClienteDao();
+        clienteDao.adicionar(cliente);
+        System.out.println(ConnectionFactory.status); //imprime o valor de status
+		
 	}
 
 }
