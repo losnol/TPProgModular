429e15c957d06b9f03d399db4306f23dcaebaf48
losnol <brenonardo@hotmail.com>
2015-12-18 11:19:35 -0200

    Criada lista de tecnicos.

--- src/ListaTecnicos.java
+++ src/ListaTecnicos.java
@@ -1,4 +1,48 @@
-//TODO
-public class ListaTecnicos {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTecnicos extends Tecnico{
+	private List<Tecnico> listaDeTecnicos;
+	public ListaTecnicos(){
+		super();
+		listaDeTecnicos = new ArrayList<Tecnico>();
+	}
+	public void inserir(Tecnico tecnico){
+		int elemento=listaDeTecnicos.indexOf(tecnico);
+		if (elemento!=-1)
+			System.out.println("Tecnico jah estah presente na lista");
+		else{
+			listaDeTecnicos.add(tecnico);
+		}
+	}
+	public void consultar (Tecnico Tecnico){
+		int elemento=listaDeTecnicos.indexOf(Tecnico);
+		if (elemento==-1)
+			System.out.println("Tecnico nao estah presente na lista");
+		else{
+			System.out.println("nome: " + listaDeTecnicos.get(elemento).getNome() +
+					"matricula: " + listaDeTecnicos.get(elemento).getMatricula() ) ;
+		}
+	}
+	public void remover(Tecnico Tecnico){
+		int elemento=listaDeTecnicos.indexOf(Tecnico);
+		if (elemento==-1)
+			System.out.println("Tecnico nao estah presente na lista");
+		else {
+			listaDeTecnicos.remove(Tecnico);
+		}
+
+	}
+	public void consultarTecnicos(){
+		for (int i=0;i<listaDeTecnicos.size();i++){
+			consultar(listaDeTecnicos.get(i));
+		}
+	}
+	public boolean existeTecnico(Tecnico Tecnico){
+		int elemento=listaDeTecnicos.indexOf(Tecnico);
+		if (elemento==-1)
+			return false;
+		return true;
+	}
 
 }
