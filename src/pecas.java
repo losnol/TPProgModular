764b2c5955d33ffad7e45b3f44e4f45d6f3b542f
losnol <brenonardo@hotmail.com>
2015-12-18 10:25:28 -0200

    Criada classe pecas.

--- src/pecas.java
+++ src/pecas.java
@@ -1,5 +1,24 @@
-//TODO
 public class pecas {
-
-
+	private String nome;
+	private double custo;
+	public pecas(){
+		nome = "NULL";
+		custo = 0;
+	}
+	public pecas(String nome, double custo){
+		this.nome = nome;
+		this.custo = custo;
+	}
+	public String getNome(){
+		return nome;
+	}
+	public double getCusto(){
+		return custo;
+	}
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	public void setCusto(double custo){
+		this.custo = custo;
+	}
 }
