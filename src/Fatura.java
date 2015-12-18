27057eff0024a0ae7f55fba6d41b090f81189911
losnol <brenonardo@hotmail.com>
2015-12-18 10:34:18 -0200

    implementada classe de fatura.

--- src/Fatura.java
+++ src/Fatura.java
@@ -1,4 +1,34 @@
-//TODO
 public class Fatura {
+	private double valor;
+	private int numeroDoRecibo;
+	private String tipoDePagamento;
+	public Fatura(){
+		valor = 0;
+		numeroDoRecibo = 0;
+		tipoDePagamento = "NULL";
+	}
+	public Fatura(double valor, int numeroDoRecibo,String tipoDePagamento){
+		this.valor = valor;
+		this.numeroDoRecibo = numeroDoRecibo;
+		this.tipoDePagamento = tipoDePagamento;
+	}
+	public double getValor(){
+		return valor;
+	}
+	public int getNumeroDoRecibo(){
+		return numeroDoRecibo;
+	}
+	public String getTipoDePagamento(){
+		return tipoDePagamento;
+	}
+	public void setValor(double valor){
+		this.valor = valor;
+	}
+	public void setNumeroDoRecibo(int numeroDoRecibo){
+		this.numeroDoRecibo = numeroDoRecibo;
+	}
+	public void setTipoDePagamento(String tipoDePagamento){
+		this.tipoDePagamento = tipoDePagamento;
+	}
 
 }
